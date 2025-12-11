12bcea580c630cd4a1c69947a5ec443fb17a5770
Greg <199975660+Gregypo085@users.noreply.github.com>
2025-12-11 17:32:05 -0500

    Initial commit

---
+++ static/script.js
@@ -0,0 +1,4 @@
+<audio autoplay loop>
+  <source src="https://www.dropbox.com/scl/fi/y3jrw93vqq0mdvea2vbjz/EDM-Sound-Design-Template-4.wav?rlkey=0qp58i2vh1cnn2kx2kzlm3zcb&st=6svy82t6&raw=1" type="audio/wav" />
+  Your browser does not support the audio element.
+</audio>
